7a70712749c6a97eee04e7916236416153661be1
Nabin Poudel <nabinpoudel788@gmail.com>
2024-09-30 15:22:28 +0545

    portfolio website  version 0.1

--- controller/mail.js
+++ controller/mail.js
@@ -1,38 +1,38 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
-const EMAIL = process.env.EMAIL;
-const PASSWORD = process.env.PASSWORD;
 
-const maileSend = (req, res) => {
-    const { name, email, subject, message } = req.body;
-    const mail = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: EMAIL,
-            pass: PASSWORD,
+const maileSend = async (req, res) => {
+    try {
+        const { name, email, message } = req.body;
+
+        const mail = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASSWORD,
+            }
+        });
+        const mailContent = {
+            from: `${email} ${name}`,
+            to: process.env.EMAIL,
+            subject: "Inquiry",
+            html: `<h1>Message from ${name}:</h1><strong>${message}</strong>`,
+            replyTo: email
         }
-    });
 
-    const mailContent = {
-        from:`${name} <${email}>`,
-        to: process.env.EMAIL,
-        subject: subject,
-        html: `<h1>${name}</h1>`,
-        text: `<p>${message}</p>`,
-        replyTo : email
-    }
-
-    mail.sendMail(mailContent, (err, sucess) => {
-        if (err) {
+        mail.sendMail(mailContent).then((sucess) => {
+            console.log(sucess);
+            return res.status(308).redirect("/contact");
+        }).catch((err) => {
             console.error(err);
-
-        }
-        console.log(sucess);
-        return res.redirect("/contact");
-
-    });
+            return res.status(404).send({ message: `failed to send email` });
+        });
+    } catch (err) {
+        res.status(203).send("failed to send reset link, Plese enter valid email address");
+        console.error(err);
+    }
 }
 
 module.exports = maileSend;
