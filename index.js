71f3bafdb8f837fbde70ed4407c4c5b4883b88fe
Nabin Poudel <nabinpoudel788@gmail.com>
2024-10-02 22:11:52 +0545

    portfolio website  version 1

--- index.js
+++ index.js
@@ -16,14 +16,33 @@ app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(compression());
-app.use(express.static("views"));
+app.use(express.static("views", {
+    setHeaders: (res, path) => {
+        res.setHeader("Cach-Control", "public, max-age=315360000");
+        res.setHeader("X-Content-Type-Options", "nosniff");
+    }
+}
+));
 
 const port = process.env.PORT || 1000;
 
 app.set("view engine", "hbs");
 
-app.post("/send", (req, res) => {
-    mail(req, res);
+app.post("/send", async (req, res) => {
+    try {
+        await mail(req, res);
+        res.status(200).send("message send sucessfully.");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("failed to send message.");
+    }
+});
+
+
+// Middleware to set Cache-Control for dynamic routes (HTML pages)
+app.use((req, res, next) => {
+    res.setHeader("Cach-Control", "no-cach, no-store, must-revalidate");
+    next();
 });
 
 // Routes
@@ -32,6 +51,7 @@ app.get("*", (req, res) => {
     const spaRoutes = ["/", "/home", "/about", "/portfolio", "/contact"];
 
     if (spaRoutes.includes(req.path)) {
+        res.setHeader("Content-Type", "text/html; charset= utf-8");
         // Render index.hbs for SPA routes
         res.render("index");
     } else {
