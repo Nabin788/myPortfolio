53af2f7e10fe93bc6e3879acffa44e5c28e27c82
Nabin Poudel <nabinpoudel788@gmail.com>
2024-10-02 16:23:11 +0545

    portfolio website  version 1

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const express = require("express");
 require("dotenv").config();
 const mail = require("./controller/mail.js");
 const cors = require('cors');
+const compression = require("compression");
 
 const app = express();
+
+// Middleware to set the X-Content-Type-Options header
+app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    next();
+});
 
 app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(compression());
+app.use(express.static("views"));
+
 const port = process.env.PORT || 1000;
 
-app.use(express.static("views"));
 app.set("view engine", "hbs");
 
 app.post("/send", (req, res) => {
@@ -20,7 +29,7 @@ app.post("/send", (req, res) => {
 // Routes
 app.get("*", (req, res) => {
     // Define your SPA routes
-    const spaRoutes = ["/", "/home", "/about" , "/portfolio", "/contact"];
+    const spaRoutes = ["/", "/home", "/about", "/portfolio", "/contact"];
 
     if (spaRoutes.includes(req.path)) {
         // Render index.hbs for SPA routes
